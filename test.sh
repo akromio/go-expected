753a3800da3081170892fc82ce7c131751b7ba46
Raúl G. González <105838447+siakro@users.noreply.github.com>
2022-10-08 18:30:52 +0200

    feat: move modules to modules dir (#2)

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-pkgs=github.com/akromio/go-expected/...
-coverage=$(go test $pkgs -covermode=count -coverprofile coverage | grep -v 'coverage: 100.0%')
+coverage=$(go test ./modules/* -covermode count -coverprofile coverage | grep -v 'coverage: 100.0%')
 if [[ "$coverage" != "" ]]; then
   go tool cover -func=coverage | grep -v "100.0%"
 fi
